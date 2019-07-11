72dc3db4d3def0303426ef30ca1b0a4efaecc12a
edward sharp <edward@edwardsharp.net>
2019-07-10 23:55:09 -0400

    seedz && notez

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # MARSH
 
-rake marsh:load_products_csv[/Users/edward/Desktop/TRASH/MARSH/sqlite/unfi_products.csv]
+A SPREE STORE
+
+_see also:_ [marshlife-art/spree_stockr](https://github.com/marshlife-art/spree_stockr)
 
 ## DEV
 
-start app and worker processes (see http://blog.daviddollar.org/2011/05/06/introducing-foreman.html):  
-`foreman start`
+to start:
+
+```sh
+bundle && \
+rake db:create && \
+rake db:migrate && \
+rake db:seed
+```
+
+start app and worker processes:  
+```sh
+foreman start
+```
+
+_see also:_ [forman](http://blog.daviddollar.org/2011/05/06/introducing-foreman.html)
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,85 @@
 
 Spree::Core::Engine.load_seed if defined?(Spree::Core)
 Spree::Auth::Engine.load_seed if defined?(Spree::Auth)
+
+Spree::Gateway::Bogus.where(
+  name: 'Credit Card',
+  description: 'Bogus payment gateway.',
+  active: true
+).first_or_create!
+
+Spree::PaymentMethod::Check.where(
+  name: 'Check',
+  description: 'Pay by check.',
+  active: true
+).first_or_create!
+
+begin
+  north_america = Spree::Zone.find_by!(name: 'North America')
+rescue ActiveRecord::RecordNotFound
+  puts 'Couldn\'t find \'North America\' zone. Did you run `rake db:seed` first?'
+  puts 'That task will set up the countries, states and zones required for Spree.'
+  exit
+end
+
+shipping_category = Spree::ShippingCategory.find_or_create_by!(name: 'Default')
+
+shipping_methods = [
+  {
+    name: 'Pickup',
+    zones: [north_america],
+    display_on: 'both',
+    shipping_categories: [shipping_category]
+  },
+  {
+    name: 'Delivery',
+    zones: [north_america],
+    display_on: 'both',
+    shipping_categories: [shipping_category]
+  }
+]
+
+shipping_methods.each do |attributes|
+  Spree::ShippingMethod.where(name: attributes[:name]).first_or_create! do |shipping_method|
+    shipping_method.calculator = Spree::Calculator::Shipping::FlatRate.create!
+    shipping_method.zones = attributes[:zones]
+    shipping_method.display_on = attributes[:display_on]
+    shipping_method.shipping_categories = attributes[:shipping_categories]
+  end
+end
+
+{
+  'Pickup' => [0, 'USD'],
+  'Delivery' => [10, 'USD']
+}.each do |shipping_method_name, (price, currency)|
+  shipping_method = Spree::ShippingMethod.find_by!(name: shipping_method_name)
+  shipping_method.calculator.preferences = {
+    amount: price,
+    currency: currency
+  }
+  shipping_method.calculator.save!
+  shipping_method.save!
+end
+
+Spree::StoreCreditCategory.find_or_create_by!(name: 'Default')
+
+north_america = Spree::Zone.find_by!(name: 'North America')
+food = Spree::TaxCategory.first_or_create!(name: 'Food')
+Spree::TaxRate.where(
+  name: 'North America',
+  zone: north_america,
+  amount: 0.01225,
+  tax_category: food
+).first_or_create! do |tax_rate|
+  tax_rate.calculator = Spree::Calculator::DefaultTax.create!
+end
+
+nonfoods = Spree::TaxCategory.first_or_create!(name: 'Non-Foods')
+Spree::TaxRate.where(
+  name: 'North America',
+  zone: north_america,
+  amount: 0.08113,
+  tax_category: nonfoods
+).first_or_create! do |tax_rate|
+  tax_rate.calculator = Spree::Calculator::DefaultTax.create!
+end
